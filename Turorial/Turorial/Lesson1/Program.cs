20698bc7f78c7b659e832f2a44ddef5671a1b097
Oleg <mycollector78@yandex.ru>
2021-05-21 18:58:22 +0600

    изменения
    изменения

---
+++ Turorial/Turorial/Lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oleg-
+ * Date: 11.05.2021
+ * Time: 14:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Lesson1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+//			for (int i = 0; i <= 5; i++){
+//				Console.WriteLine(i + "\n");
+//			}
+			
+//			int i = 0;
+//			while (i <= 6) {
+//				Console.WriteLine(i + "\n");
+//				i++;
+//			}
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
